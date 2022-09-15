7b8eedd55e87e26993823c141b50965ede3b9b1e
Zhai Jingbo <201240105@smail.nju.edu.cn>
2022-09-15 07:14:29 -0700

    receiver finished.

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -5,15 +5,26 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    TCPHeader _header=seg.header();
+    if(!_syn^_header.syn)return;
+    if(!_syn){
+        _syn=true;
+        _seqno=_header.seqno;
+    }
+    _fin=_header.fin;
+    uint64_t abs_seqno=unwrap(_header.seqno,_seqno,_reassembler.stream_out().bytes_written());
+    uint64_t index=abs_seqno-(!_header.syn);
+    _reassembler.push_substring(seg.payload().copy(),index,_fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if(!_syn)return {};
+    return wrap(stream_out().bytes_written()+_syn+_reassembler.stream_out().input_ended(),_seqno);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return _capacity-_reassembler.stream_out().buffer_size();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -19,6 +19,8 @@ class TCPReceiver {
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
+    WrappingInt32 _seqno{0};
+    bool _syn{false},_fin{false};
 
   public:
     //! \brief Construct a TCP receiver
