1aa420794fb75eb5ad29159bbe11e726405ec341
Jingbo Zhai <jingbozhai@smail.nju.edu.cn>
2024-09-30 14:14:46 +0800

    finished wrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,13 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>( n + zero_point.raw_value_ ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  int32_t tmp = raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
+  int64_t ans = checkpoint + tmp;
+  return ans >= 0 ? ans : ( ans + ( 1ul << 32 ) );
 }
